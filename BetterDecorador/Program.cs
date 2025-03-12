4ebc96f78ba778254d8932898cde5c3cd4296ca0
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-12 21:58:57 +0000

    1st exercise, tamper with args

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static string Decor(string s , char dec, int num)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+        private static void Main(string[] args)
+        {
+            Console.WriteLine (Decor("Good Morning!", '*', 3));
+            Console.WriteLine (Decor("Good Evening !", '.',2));
+        }
+    }
+}
