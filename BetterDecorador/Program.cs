ecfb4fef101c8afc33e1b510991d418babef6e36
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-12 23:02:25 +0000

    1st exercise, works with args

--- BetterDecorador/Program.cs
+++ BetterDecorador/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace BetterDecorador
 {
@@ -6,15 +7,36 @@ namespace BetterDecorador
     {
         private static string Decor(string s , char dec, int num)
         {
+            /*
             string border = new string(dec,num);
-            
-            return $"{border} {s} {border}";
+            return $"{border}{s}{border}";
+            */
+
+            int n = 0;
+            s = " " + s + " ";
+            while(n != num)
+            {
+                s = dec + s + dec;
+                ++n;
+            }
+            return s;
+
         }
         private static void Main(string[] args)
         {
-            
+            /*
             Console.WriteLine (Decor("Good Morning!", '*', 3));
             Console.WriteLine (Decor("Good Evening !", '.',2));
+            */
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Specify Args");
+            }
+            else
+            {
+               Console.WriteLine(Decor(args[0],char.Parse(args[1]), 
+            int.Parse(args[2]))); 
+            }
         }
     }
 }
