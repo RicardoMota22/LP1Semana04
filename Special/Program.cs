a9623145527598c6b8da12da7bc0478503526dad
RicardoMota22 <ricardomota.artist@gmail.com>
2025-03-13 22:09:24 +0000

    4th exercise

---
+++ Special/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+
+namespace Special
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Insert n-th number of the special sequence: ");
+            int special = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(special));
+            GetSpecial(0);
+        }
+        /*
+        private static ulong Factorial(ulong n)
+        {
+        ulong fact ;
+        if (n==1) // Caso base
+        fact = 1;
+        else // Caso geral
+        fact =n * Factorial(n - 1);
+        return fact ;
+        }
+        */
+        private static int GetSpecial(int n)
+        {
+            int special;
+            if(n == 0)
+            {
+                special = 0;
+            }
+            else if (n == 1)
+            {
+                special = 1;
+            }
+            else
+            {
+                special = GetSpecial(n-1) + (2 * GetSpecial(n-2));
+            }
+            return special;
+        }
+       
+
+    }
+}
